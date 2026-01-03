29201406aaea302b1e732fc2d2d55d219f9797db
himanshu b sarvaiya <himanshusarvaiya77@gmail.com>
2026-01-03 14:27:11 +0530

    Add files via upload

---
+++ excercise/2_triangle.py
@@ -0,0 +1,12 @@
+# write a programe to calculate the area of triangle
+# formula = 1/2 * base * height
+
+base = 0
+height = 0
+
+base = float(input("Enter the base:"))
+height = float(input("Enter the height:"))
+
+area = 0.5 * base * height
+
+print("Area of triangle is :", area)
